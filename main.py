5b887bef97f40059cbd27a6d9ba9a898ea077f10
quantwoori <sangil.bae@wooriam.co.kr>
2022-03-15 10:05:02 +0900

    <dev> date fix

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from util.UTIL_monkeypatch import no_ssl_verification
 
 
 with no_ssl_verification():
-    d0 = datetime(2010, 1, 1)
+    d0 = datetime(2022, 1, 1)
     d1 = datetime(2022, 2, 28)
     ds = search_dates(d0, d1)
 
-    krx = KRXIndexComposite(srch_key='코스피 소형주', date=ds)
-    
+    krx = KRXIndexComposite(srch_key='코스피 200', date=ds)
